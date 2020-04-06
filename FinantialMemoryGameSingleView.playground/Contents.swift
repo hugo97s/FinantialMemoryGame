7d0513a067375f28868fe85fa358f6978f0b7d18
Hugo Santos <hugo@Hugos-MacBook-Pro.local>
2020-04-05 23:29:06 -0300

    tela inicial

--- FinantialMemoryGameSingleView.playground/Contents.swift
+++ FinantialMemoryGameSingleView.playground/Contents.swift
@@ -1,21 +1,66 @@
-//: A UIKit based Playground for presenting user interface
-  
 import UIKit
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
+class MyViewController: UIViewController {
     override func loadView() {
         let view = UIView()
         view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
         
-        view.addSubview(label)
+        let image = #imageLiteral(resourceName: "Group 2.png")
+        let imageView = UIImageView(image: image)
+        imageView.frame = CGRect(x: 244, y: 255, width: 190, height: 190)
+
+        view.addSubview(imageView)
         self.view = view
+        
+        let image2 = #imageLiteral(resourceName: "Group 13.png")
+        let imageView2 = UIImageView(image: image2)
+        imageView2.frame = CGRect(x: 0, y: 0, width: 1366, height: 117)
+
+        view.addSubview(imageView2)
+        self.view = view
+        
+        let image3 = #imageLiteral(resourceName: "Group 2.png")
+        let imageView3 = UIImageView(image: image3)
+        imageView3.frame = CGRect(x: 507, y: 255, width: 190, height: 190)
+
+        view.addSubview(imageView3)
+        self.view = view
+        
+        let image4 = #imageLiteral(resourceName: "Group 2.png")
+        let imageView4 = UIImageView(image: image4)
+        imageView4.frame = CGRect(x: 780, y: 255, width: 190, height: 190)
+
+        view.addSubview(imageView4)
+        self.view = view
+        
+        let image5 = #imageLiteral(resourceName: "Group 2.png")
+        let imageView5 = UIImageView(image: image5)
+        imageView5.frame = CGRect(x: 244, y: 531, width: 190, height: 190)
+
+        view.addSubview(imageView5)
+        self.view = view
+        
+        let image6 = #imageLiteral(resourceName: "Group 2.png")
+        let imageView6 = UIImageView(image: image6)
+        imageView6.frame = CGRect(x: 507, y: 531, width: 190, height: 190)
+
+        view.addSubview(imageView6)
+        self.view = view
+        
+        
+        let image7 = #imageLiteral(resourceName: "Group 2.png")
+        let imageView7 = UIImageView(image: image7)
+        imageView7.frame = CGRect(x: 780, y: 531, width: 190, height: 190)
+
+        view.addSubview(imageView7)
+        self.view = view
+        
     }
+    
+    
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+let vc = MyViewController(screenType: .ipadPro12_9, isPortrait: false)
+PlaygroundPage.current.liveView = vc.scale(to: 0.4)
+
